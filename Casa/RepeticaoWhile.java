8397ebf33f7f109c920132c8b1adb811765056f5
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-04-28 22:10:53 -0300

    Atualizacao

---
+++ Casa/RepeticaoWhile.java
@@ -0,0 +1,9 @@
+class RepeticaoWhile{
+    public static void main(String arg){
+        int i = 1;// inicialicao do i
+        while(i<=10){//encerramento em 10
+            System.out.println("i =" +i+ " / Parâmetro: " + arg);//imprime o valor da variavel i
+            i++;//interacao para adicionar 1 em 1
+        }
+    }
+}
